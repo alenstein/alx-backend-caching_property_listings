8123766ac4d29501666d206696d8b9a51df7bd74
Allen Sibanda <sibandallen@gmail.com>
2025-11-05 00:15:50 +0200

    fix: docker-compose file configs, made migrations

--- alx_backend_caching_property_listings/settings.py
+++ alx_backend_caching_property_listings/settings.py
@@ -9,6 +9,11 @@ env = environ.Env(
     DEBUG=(bool, False)
 )
 
+# Take environment variables from .env file
+environ.Env.read_env(BASE_DIR / '.env')
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
@@ -20,6 +25,7 @@ ALLOWED_HOSTS = list(env('ALLOWED_HOSTS'))
 
 
 # Application definition
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
