3ec8bb05959d2630e71da63daa9811971e095235
Allen Sibanda <sibandallen@gmail.com>
2025-11-05 00:26:25 +0200

    task: implement Cache Property List View

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.core.serializers import serialize
+from django.http import HttpResponse
+from django.views.decorators.cache import cache_page
+from .models import Property
 
-# Create your views here.
+@cache_page(60 * 15) # Cache for 15 minutes
+def property_list(request):
+    properties = Property.objects.all()
+    data = serialize("json", properties)
+    return HttpResponse(data, content_type="application/json")
