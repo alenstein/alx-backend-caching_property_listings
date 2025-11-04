e08870e69dfe0264ddf894e1d00a5d1ac446de84
Allen Sibanda <sibandallen@gmail.com>
2025-11-05 00:23:57 +0200

    fix: django settings configs

--- alx_backend_caching_property_listings/settings.py
+++ alx_backend_caching_property_listings/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
         'NAME': env('DB_NAME'),
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': env('DB_PORT'),
+        'HOST': env('DB_HOST', 'localhost'),
+        'PORT': env('DB_PORT','5432'),
     }
 }
 
