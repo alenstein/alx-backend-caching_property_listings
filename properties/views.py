0658b153521df425956d8f640fdcca1bd3960aae
Allen Sibanda <sibandallen@gmail.com>
2025-11-05 00:28:45 +0200

    fix: return JsonResponce instead of HTTPResponce

--- properties/views.py
+++ properties/views.py
@@ -1,5 +1,6 @@
 from django.core.serializers import serialize
 from django.http import HttpResponse
+from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
 from .models import Property
 
@@ -7,4 +8,4 @@ from .models import Property
 def property_list(request):
     properties = Property.objects.all()
     data = serialize("json", properties)
-    return HttpResponse(data, content_type="application/json")
+    return JsonResponse(data, content_type="application/json")
